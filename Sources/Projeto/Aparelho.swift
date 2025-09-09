60b1c7c06ed922f1c027b6d014cccea7dd3acb1e
Theo Espósito <thesposito.estudos@gmail.com>
2025-09-09 18:26:38 +0000

    Atualizando classe Aparelho

--- Sources/Projeto/Aparelho.swift
+++ Sources/Projeto/Aparelho.swift
@@ -1,11 +1,12 @@
-// class Aparelho: Manutencao{
-//     private let nomeItem: String
-//     private var dataUltimaManutencao: String
-//     func realizarManutencao() -> Bool{
-//         print("Realizando manutenção do aparelho \(nomeItem)!")
-//         return true
-//     }
-//     init(nomeItem: String){
-//         self.nomeItem = nomeItem
-//     }
-// }
+class Aparelho: Manutencao{
+    private let nomeItem: String
+    private var dataUltimaManutencao: String
+    func realizarManutencao() -> Bool{
+        print("Realizando manutenção do aparelho \(nomeItem)!")
+        return true
+    }
+    init(nomeItem: String){
+        self.nomeItem = nomeItem
+        self.dataUltimaManutencao = "Nenhuma"
+    }
+}
