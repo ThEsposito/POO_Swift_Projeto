22830d49c8b6f207ca49e20850a1c66afcb2ebef
Theo Espósito <thesposito.estudos@gmail.com>
2025-09-09 18:15:40 +0000

    Testando classes

--- Sources/Aula_1/main.swift
+++ Sources/Aula_1/main.swift
@@ -4,3 +4,8 @@ let p1: Plano = Plano(nome: "rerere")
 
 let aluno: Aluno = Aluno(nome: "Pedro", email:"pedro@gmail.com", matricula: "6 meses", plano: p1)
 
+let inst = Instrutor(nome: "TheoPai", email: "hteozinhgames@gmail.com", especialidade: "Minecraft")
+let aulaColetiva = AulaColetiva(nome: "Como zerar em 5 minutor", instrutor: inst)
+
+print(aulaColetiva.inscrever(aluno: aluno))
+print(aulaColetiva.inscrever(aluno: aluno))
