b8f397818c089dc1d0b1b2946d8ebc11bc27657d
Theo Espósito <thesposito.estudos@gmail.com>
2025-09-09 18:22:10 +0000

    Atualizando estrutura de pastas

--- Package.swift
+++ Package.swift
@@ -5,24 +5,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Poo_Swift+Projeto", // Aqui você deve alterar para o nome do seu repositorio
+    name: "Poo_Swift_Projeto",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "Aula_1"
-        ),
-        .executableTarget(
-            name: "Aula_2"
-        ),
-        .executableTarget(
-            name: "Aula_3"
-        ),
-        .executableTarget(
-            name: "Aula_4"
-        ),
-        .executableTarget(
-            name: "Aula_5"
+            name: "Projeto"
         ),
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,7 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-var nome = "Seu Nome"
-let sobrenome = "Seu Sobrenome"
+import Aula_1
 
-print("Bem-vindo(a), \(nome) \(sobrenome)!")
+let p1: Plano = Plano(nome: "rerere")
+
+let aluno: Aluno = Aluno(nome: "Pedro", email:"pedro@gmail.com", matricula: "6 meses", plano: p1)
+
+let inst = Instrutor(nome: "TheoPai", email: "hteozinhgames@gmail.com", especialidade: "Minecraft")
+let aulaColetiva = AulaColetiva(nome: "Como zerar em 5 minutor", instrutor: inst)
+
+print(aulaColetiva.inscrever(aluno: aluno))
+print(aulaColetiva.inscrever(aluno: aluno))
